0f2d3a7ba6c295744c7a9197512f57998229be35
IhorYu <ihoryukhnovskyi@gmail.com>
2022-09-28 15:25:45 +0200

    reset password feature

--- accounts/views.py
+++ accounts/views.py
@@ -20,7 +20,7 @@ def check_role_vendor(user):
         raise PermissionDenied
 
 
-# Restrict the customer from asccessing the vendor page
+# Restrict the customer from accessing the vendor page
 
 def check_role_customer(user):
     if user.role == 2:
@@ -32,7 +32,7 @@ def check_role_customer(user):
 def register_user(request):
     if request.user.is_authenticated:
         messages.warning(request, 'You are already logged in')
-        return redirect('dashboard')
+        return redirect('my_account')
     elif request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
@@ -42,7 +42,9 @@ def register_user(request):
             user.save()
 
             # Send verification email
-            send_verification_email(request, user)
+            mail_subject = 'Please activate your account'
+            email_template = 'accounts/email/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
             messages.success(request, 'Your account has been registered')
             return redirect('register_user')
         else:
@@ -82,7 +84,9 @@ def register_vendor(request):
             vendor.save()
 
             # Send verification email
-            send_verification_email(request, user)
+            mail_subject = 'Please activate your account'
+            email_template = 'accounts/email/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
             messages.success(request, 'Your account has been registered successfully! Please wait for the approval.')
             return redirect('register_vendor')
 
@@ -162,3 +166,60 @@ def customer_dashboard(request):
 @user_passes_test(check_role_vendor)
 def vendor_dashboard(request):
     return render(request, 'accounts/vendor_dashboard.html')
+
+
+def forgot_password(request):
+    if request.method == "POST":
+        email = request.POST['email']
+
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email__exact=email)
+
+            # Send reset password email
+            mail_subject = 'Reset your password'
+            email_template = 'accounts/emails/reset_password_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
+
+            messages.success(request, 'Password reset link has been send to your email address')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist')
+            return redirect('forgot_password')
+
+    return render(request, 'accounts/forgot_password.html')
+
+
+def reset_password_validate(request, uidb64, token):
+    # Validate the user by decoding the token and user pk
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.info(request, 'Please reset your password')
+        return redirect('reset_password')
+    else:
+        messages.error(request, 'This link has been expired!')
+        return redirect('my_account')
+
+
+def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            pk = request.session.get('uid')
+            user = User.objects.get(pk=pk)
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do mot match')
+            return redirect('reset_password')
+    return render(request, 'accounts/reset_password.html')
