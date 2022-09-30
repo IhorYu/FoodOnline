69c61c33eaded29dbcd41273419aee5bc7f5e4b6
IhorYu <ihoryukhnovskyi@gmail.com>
2022-10-01 01:15:43 +0200

    added autocomplete address by google

--- FoodOnline_main/settings.py
+++ FoodOnline_main/settings.py
@@ -65,7 +65,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
 
-                'accounts.context_processors.get_vendor'
+                'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -149,3 +150,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+GOOGLE_API_KEY = config('GOOGLE_API_KEY')
