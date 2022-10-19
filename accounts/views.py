90937c5c88eea9dc67223a1a38c19998940addde
IhorYu <ihoryukhnovskyi@gmail.com>
2022-10-19 14:44:23 +0200

    Customer my orders

--- accounts/views.py
+++ accounts/views.py
@@ -164,10 +164,12 @@ def my_account(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
 def customer_dashboard(request):
-    orders = Order.objects.filter(user=request.user, is_ordered=True)[:5]
+    orders = Order.objects.filter(user=request.user, is_ordered=True)
+    recent_orders = orders[:5]
     context = {
         'orders': orders,
         'orders_count': orders.count(),
+        'recent_orders': recent_orders,
     }
     return render(request, 'accounts/customer_dashboard.html', context)
 
--- customers/urls.py
+++ customers/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', AccountViews.customer_dashboard, name='customer'),
     path('profile/', views.customer_profile, name='customer_profile'),
+    path('my_orders/', views.my_orders, name='customer_my_orders'),
 ]
